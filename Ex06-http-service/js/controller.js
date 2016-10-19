b4dde7b964d66293f73026c0a31f8a4553633238
oswaldosalazar <oswaldo.r.salazar@gmail.com>
2016-10-18 20:30:08 -0600

    Finished 6

--- Ex06-http-service/js/controller.js
+++ Ex06-http-service/js/controller.js
@@ -2,19 +2,48 @@ app.controller("HomeController", function($scope, $http) {
     $scope.view = {};
     $scope.view.welcome = "Home"
 
-    $http.get('https://api.github.com/zen')
-    .then(function(data){
-        $scope.view.zenData = data;
-    });
+    // $http.get('https://api.github.com/zen')
+    // .then(function(data){
+    //     $scope.view.zenData = data;
+    // });
+
+
+    // $http.get('itunes.json')
+    // .then(function(artistData){
+    //     $scope.view.itunes = artistData.data.results;
+    // });
 
     // $http.get('https://itunes.apple.com/search?term=lorde')
     // .then(function(data){
     //     $scope.view.itunes = data;
+    // }, function error(response) {
+    //     $scope.view.error = response;
     // });
 
-    $http.get('itunes.json')
-    .then(function(artistData){
-        $scope.view.itunes = artistData.data.results;
-    });
+    $http.get('https://messagehttpservice.herokuapp.com/messages')
+    .then(function(data) {
+        $scope.view.chatApp = data;
+    })
+
+    $http.post('https://messagehttpservice.herokuapp.com/messages')
+    .then(function(data) {
+
+    })
+
+    $http.post('https://messagehttpservice.herokuapp.com/messages', {
+        message: {
+            name: "Os",
+            content: "I love carpet."
+        }
+    })
+   .then(
+       function(response){
+           $scope.view.chatApp = response;
+       },
+       function(error){
+           $scope.view.error = error;
+       }
+    );
+
 
 });
