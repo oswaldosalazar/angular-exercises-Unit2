29d49142fed8cae799225b210eee3d96d0fe8a59
oswaldosalazar <oswaldo.r.salazar@gmail.com>
2016-11-18 20:56:13 -0700

    Added s to http

--- Ex12-shopping-cart-app/js/service.js
+++ Ex12-shopping-cart-app/js/service.js
@@ -2,7 +2,6 @@ app.factory("cartService", function() {
     return {
         cart: {},
         receiveCart: function (cart) {
-            console.log(cart);
             this.cart = cart;
         }
     }
